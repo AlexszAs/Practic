b557c450d56f21cccb37ef3e903f32b92f51416e
AlexszAs <Azazin4eg@gmail.com>
2022-08-28 11:30:41 +0200

    commit 1

--- practic/js/script.js
+++ practic/js/script.js
@@ -221,3 +221,44 @@
 
 // const test = '12.2px';
 // console.log(parseInt(test))
+
+// function First () {
+//     setTimeout(function() {
+//         console.log(1);   
+//     }, 500);
+// }
+
+// function Second () {
+//     console.log(2);
+// }
+
+// First();
+// Second();
+
+// function learnJS(lang, callback) {
+//     console.log(`I learn: ${lang}`);
+//     callback();
+// }
+
+// function done () {
+//     console.log('I finish thiss lesson');
+// }
+
+// learnJS('JavaScript', done);
+
+// 
+
+const arr = [1, 2, 3, 4, 5, 8];
+
+// arr.pop();
+// arr.push(20);
+// console.log(arr);
+
+// for(let i = 0; i < arr.length; i++) {
+//     console.log(arr[i]);
+// }
+
+
+for (let value of arr) {
+    console.log(value);
+}
