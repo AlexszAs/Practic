76ca0accd8aa30cfc439e585eb08cff06de440a6
AlexszAs <Azazin4eg@gmail.com>
2022-08-29 15:02:07 +0200

    commit2

--- practic/js/script.js
+++ practic/js/script.js
@@ -249,16 +249,33 @@
 // 
 
 const arr = [1, 2, 3, 4, 5, 8];
+arr.sort(compareNum);
+console.log(arr);
 
-// arr.pop();
-// arr.push(20);
-// console.log(arr);
-
-// for(let i = 0; i < arr.length; i++) {
-//     console.log(arr[i]);
-// }
-
-
-for (let value of arr) {
-    console.log(value);
+function compareNum(a, b) {
+    return a - b;
 }
+
+
+// // arr.pop();
+// // arr.push(20);
+// // console.log(arr);
+
+// // for(let i = 0; i < arr.length; i++) {
+// //     console.log(arr[i]);
+// // }
+
+
+// // for (let value of arr) {
+// //     console.log(value);
+// // }
+
+// // 
+// arr.forEach(function(item, i, arr) {
+//     console.log(`${i}: ${item} in massive ${arr}`);
+// })
+
+// const str = prompt("", "");
+// const products = str.split(", ");
+// products.sort();
+// console.log(products.join('; '));
