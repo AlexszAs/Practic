ab94a03b68eaa56925a53aa6458b121ee75634f5
AlexszAs <Azazin4eg@gmail.com>
2022-09-05 06:21:12 +0200

    commit_3

--- practic/js/script.js
+++ practic/js/script.js
@@ -248,13 +248,13 @@
 
 // 
 
-const arr = [1, 2, 3, 4, 5, 8];
-arr.sort(compareNum);
-console.log(arr);
+// const arr = [1, 2, 3, 4, 5, 8];
+// arr.sort(compareNum);
+// console.log(arr);
 
-function compareNum(a, b) {
-    return a - b;
-}
+// function compareNum(a, b) {
+//     return a - b;
+// }
 
 
 // // arr.pop();
@@ -279,3 +279,89 @@ function compareNum(a, b) {
 // const products = str.split(", ");
 // products.sort();
 // console.log(products.join('; '));
+
+// function copy(mainObj) {
+//     let objCopy = {};
+//     let key;
+//     for (key in mainObj) {
+//         objCopy[key] = mainObj [key];
+//     }
+//     return objCopy;
+// }
+
+// const numbers = {
+//     a: 2,
+//     b: 5,
+//     c: {
+//         x: 7,
+//         y: 4
+//     }
+// };
+
+// const newNumbers = copy(numbers);
+
+// newNumbers.a = 10;
+
+// console.log(newNumbers);
+
+
+// const add = {
+//     d: 17,
+//     e: 20
+// };
+
+// console.log(Object.assign(numbers, add));
+
+// const oldArray = ['a', 'b', 'c'];
+// const newArray = oldArray.slice();
+
+// newArray[1] = 'asdsgdsdfse';
+// console.log(newArray);
+// console.log(oldArray);
+
+
+// const video = ['YouTube', 'Vimeo', 'RuTube'],
+//     blogs = ['WordPress', 'LiveJournal', 'Blogger'],
+//     internet = [...video, ...blogs, 'VK', 'FaceBook'];
+
+// console.log(internet);
+
+// function log (a, b, c) {
+//     console.log(a);
+//     console.log(b);
+//     console.log(c);
+// }
+
+// const num = [2, 5, 7];
+// log(...num);
+
+// const array = ['a', 'b'];
+//  const newArray = [...array];
+
+//  const q = {
+//     one:1,
+//     two:2
+//  };
+
+//  const newObj = {...q};
+
+// const personalPlanPeter = {
+//     name: "Peter",
+//     age: "29",
+//     skills: {
+//         languages: ['ru', 'eng'],
+//         programmingLangs: {
+//             js: '20%',
+//             php: '10%'
+//         },
+//         exp: '1 month'
+//     }
+// };
+
+// function showExperience(plan) {
+
+// }
+
+// function showProgrammingLangs(plan) {
+
+// }
